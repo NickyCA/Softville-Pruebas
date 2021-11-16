22be12af5ef5395ca7889f96a348992f9eb1e8d2
Nicole <nicky260899@gmail.com>
2021-11-16 17:52:27 -0600

    tests del create, se quitaron los comentarios en el appointment controller

--- ElectronicNotebook/ElectronicNotebookTesting/Controllers/AppointmentControllerTest.cs
+++ ElectronicNotebook/ElectronicNotebookTesting/Controllers/AppointmentControllerTest.cs
@@ -23,5 +23,22 @@ namespace ElectronicNotebookTesting.Controllers
             ViewResult result = controller.Index() as ViewResult;
             Assert.AreEqual("Index", result.ViewName);
         }
+
+        [TestMethod]
+        public void TestCreateView()
+        {
+            AppointmentController controller = new AppointmentController();
+            ViewResult result = controller.Create() as ViewResult;
+            Assert.AreEqual("Create", result.ViewName);
+        }
+
+        [TestMethod]
+        public void TestCreateNotNull()
+        {
+            AppointmentController controller = new AppointmentController();
+            ViewResult result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
     }
 }
